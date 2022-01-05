295185da8ce48e642acdba47b785b513f0368c1a
Jongchan <ahnchan2@fnfcorp.com>
2022-01-05 17:19:32 +0900

    swagger 라이브러리 설치 및 main.ts 설정, @nestjs/swagger swagger-ui-express

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { MyLogger } from './common/utils/logger/logger.service';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -11,6 +12,14 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const logger = app.get(MyLogger);
+  const config = new DocumentBuilder()
+    .setTitle('nestjs-tst-boilerplate')
+    .setDescription('The nestjs-tst-boilerplate API description')
+    .setVersion('0.0.1')
+    .addTag('tag')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(configService.get<number>('app.port'));
   logger.log(`Application running on [${configService.get<string>('app.env')}] enviroment. ${await app.getUrl()}`);
